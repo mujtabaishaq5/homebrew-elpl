331cb8b681a5b9877ee998742455357f085d0e96
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2025-09-23 21:10:46 +0200

    Update elpl.rb

--- elpl.rb
+++ elpl.rb
@@ -1,9 +1,9 @@
 class Elpl < Formula
   desc "ELPL programming language"
-  homepage "https://mujtabaishaq5.github.io/ELPL-Official/"
-  url "https://mujtabaishaq5.github.io/ELPL-Official/ELPL.zip"
+  homepage "https://github.com/mujtabaishaq5/homebrew-elpl"
+  url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/elpl.zip"
   version "6.7.2"
-  sha256 "6dcec4ffd5a1d2a52a1969d2b2ca221d7037036f4d92a7b9962081ec9292bc64"
+  sha256 "0e45a761fe790a2f3960291a3515fb3acb4a911e6f46381ddb918aeed368c6f2"
   license "MIT"
 
   def install
