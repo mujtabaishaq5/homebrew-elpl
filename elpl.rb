d5931dc6af03f52c8264b784c18c20588985fa76
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2025-09-23 20:38:05 +0200

    Update elpl.rb

--- elpl.rb
+++ elpl.rb
@@ -1,7 +1,8 @@
 class Elpl < Formula
   desc "ELPL programming language"
-  homepage "https://example.com" # your website
-  url "https://mujtabaishaq5.github.io/ELPL-Official/ELPL.zip" # binary URL
+  homepage "https://example.com"
+  url "https://mujtabaishaq5.github.io/ELPL-Official/ELPL.zip"
+   version "6.7.2"  
   sha256 "6dcec4ffd5a1d2a52a1969d2b2ca221d7037036f4d92a7b9962081ec9292bc64"
   license "MIT"
 
