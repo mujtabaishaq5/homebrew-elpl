1c94e0b42e66927ed4f2bcaec759a763b0b470b5
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2025-09-23 20:41:11 +0200

    Update elpl.rb

--- elpl.rb
+++ elpl.rb
@@ -2,7 +2,7 @@ class Elpl < Formula
   desc "ELPL programming language"
   homepage "https://example.com"
   url "https://mujtabaishaq5.github.io/ELPL-Official/ELPL.zip"
-   version "6.7.2"  
+  version "6.7.2"
   sha256 "6dcec4ffd5a1d2a52a1969d2b2ca221d7037036f4d92a7b9962081ec9292bc64"
   license "MIT"
 
@@ -11,6 +11,6 @@ class Elpl < Formula
   end
 
   test do
-    system "#{bin}/elpl", "--version"
+    system "#{bin}/elpl", "--v"
   end
 end
