f6186648e4450abc8070230d32a7f916f459112b
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2025-09-23 20:43:50 +0200

    Update elpl.rb

--- elpl.rb
+++ elpl.rb
@@ -1,6 +1,6 @@
 class Elpl < Formula
   desc "ELPL programming language"
-  homepage "https://example.com"
+  homepage "https://mujtabaishaq5.github.io/ELPL-Official/"
   url "https://mujtabaishaq5.github.io/ELPL-Official/ELPL.zip"
   version "6.7.2"
   sha256 "6dcec4ffd5a1d2a52a1969d2b2ca221d7037036f4d92a7b9962081ec9292bc64"
