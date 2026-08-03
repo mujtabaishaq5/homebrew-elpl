4c1b2b9b364bd21f4403cc675bed71e0629a1341
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2026-08-03 14:26:36 +0200

    Update ELPL download URL and SHA256 checksum

--- elpl.rb
+++ elpl.rb
@@ -1,9 +1,9 @@
 class Elpl < Formula
   desc "ELPL programming language"
   homepage "https://github.com/mujtabaishaq5/homebrew-elpl"
-  url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/download/programminglanguage/ELPL.zip"
+  url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/download/programminglanguage/ELPLN.zip"
   version "7.1.3"
-  sha256 "bca9816b0dc666433e71a350d083ff9deb42eb3f850ae314bf73301b90ac0405"
+  sha256 "85726cff4545e5f89336f552d4cc5ab00dd11577ddf45a7c9d1bbdd95e4c8364"
   license "MIT"
 
   def install
