8823ab29586c7eb388d6744b3a0dbab1c5953cc2
Syed Muhammad Mujtaba Ishaq <44325365+mujtabaishaq5@users.noreply.github.com>
2026-09-01 21:19:54 +0200

    Update SHA256 checksums in elpl.rb
    Updated SHA256 checksums for macOS and Linux builds.

--- elpl.rb
+++ elpl.rb
@@ -7,18 +7,15 @@ class Elpl < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      # Apple Silicon (Built via GitHub Actions)
       url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/download/programminglanguage/ELPL-mac-arm64.zip"
-      sha256 "aa8adda40944d988852a1e091a96c396b2803ba71a9b07d34987579eaee6711e"
+      sha256 "UPDATE_AFTER_RELEASE"
     else
-      # Intel Mac (Built locally on your 2018 MacBook Pro)
       url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/download/programminglanguage/ELPL-mac-intel64.zip"
-      sha256 "5f6703bde2885f32e78749e5eab930702dccb9be8382ab0370f827e68239c298"
+      sha256 "UPDATE_AFTER_RELEASE"
     end
   elsif OS.linux?
-    # Linux x64
     url "https://github.com/mujtabaishaq5/homebrew-elpl/releases/download/programminglanguage/elpl-linux-x64.tar.gz"
-    sha256 "e892b000a0f46d4f0e6356eb317588289f3c51e041cf18d30563c54424702606"
+    sha256 "UPDATE_AFTER_RELEASE"
   end
 
   def install
